c6f65d44c74cdd2b627a6a5d13cd853fc4198dec
joyiee <huangjiawei@sjtu.edu.cn>
2025-02-25 20:24:45 +0800

    add message defination and implementation

--- include/message.h
+++ include/message.h
@@ -1,4 +1,52 @@
 #ifndef MESSAGE_H
 #define MESSAGE_H
+#include <string>
+#include<iostream>
+#include <vector>
+class Message{
+public:
+    //Constructors
+    Message(std::string type, std::string sender, std::string receiver);
+    Message();
+
+    //Getter functions
+    std::string get_type() const;
+    std::string get_sender() const;
+    std::string get_recevier() const;
+    std::string get_time() const;
+
+    virtual void print(std::ostream &os) const; 
+
+    friend std::ostream& operator<<(std::ostream &os, const Message &msg);
+
+private:
+    std::string type;
+    std::string sender;
+    std::string receiver;
+    std::string time;
+};
+
+class TextMessage : public Message
+{
+private:
+    std::string text;
+public:
+    TextMessage(std::string text, std::string sender, std::string receiver);
+    void print(std::ostream &os) const override;
+    std::string get_text() const;
+private:
+    std::string text;
+};
+
+class VoiceMessage : public Message
+{
+private:
+    std::vector<unsigned char> voice;
+public:
+    VoiceMessage(std::string sender, std::string receiver);
+    void print(std::ostream &os) const override;
+
+    std::vector<unsigned char> get_voice() const;
+};
 
 #endif //MESSAGE_H
--- src/message.cpp
+++ src/message.cpp
@@ -1,1 +1,81 @@
 #include "message.h"
+#include <ctime>
+
+Message::Message(std::string type, std::string sender, std::string receiver) : type(type), sender(sender), receiver(receiver)
+{
+    auto now = std::time(nullptr);
+    time = std::ctime(&now);
+    time.pop_back();
+}
+
+Message::Message():type("Unkown"), sender("Unkown"), receiver("Unkown"), time("Unkown"){};
+
+std::string Message::get_type() const
+{
+    return type;
+}
+
+std::string Message::get_sender() const
+{
+    return sender;
+}
+
+std::string Message::get_recevier() const
+{
+    return receiver;
+}
+
+std::string Message::get_time() const
+{
+    return time;
+}
+
+void Message::print(std::ostream &os) const
+{
+    os << "*************************\n" \
+    << sender << " -> " << receiver << "\n" \
+    << "message type: " << type << "\n" \
+    << "message time: " << time << \
+    "\n*************************" << std::endl;
+}
+
+std::ostream& operator<<(std::ostream &os, const Message &msg)
+{
+    msg.print(os);
+    return os;
+}
+
+TextMessage::TextMessage(std::string text, std::string sender, std::string receiver):Message("text", sender, receiver), text(text) {};
+void TextMessage::print(std::ostream &os) const
+{
+    os << "*************************\n" \
+    << get_sender() << " -> " << get_recevier() << "\n" \
+    << "message type: " << get_type() << "\n" \
+    << "message time: " << get_time()  \
+    << "text: " << text << \
+    "\n*************************" << std::endl;
+}
+
+std::string TextMessage::get_text() const{
+    return text;
+}
+
+VoiceMessage::VoiceMessage(std::string sender, std::string receiver):Message("voice", sender, receiver){};
+
+void VoiceMessage::print(std::ostream &os) const
+{
+    os << "*************************\n" \
+    << get_sender() << " -> " << get_recevier() << "\n" \
+    << "message type: " << get_type() << "\n" \
+    << "message time: " << get_time() \
+    << "voice:";
+    for(auto word : voice)
+    {
+        os << " " << static_cast<int>(word);
+    }
+    os << "\n*************************" << std::endl;
+}
+
+std::vector<unsigned char> VoiceMessage::get_voice() const{
+    return voice;
+}
